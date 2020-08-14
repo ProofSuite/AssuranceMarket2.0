6dc7844cb5b3271a0024e6e4c4351f674ed08f79
mulili <mulili.nzuki@gmail.com>
2020-08-14 23:31:38 +0300

    initial scaffolding

--- contracts/amp.sol
+++ contracts/amp.sol
@@ -25,17 +25,72 @@ pragma experimental ABIEncoderV2;
 pragma solidity 0.6.6;
 
 
+interface OrFeedInterface {
+  function getExchangeRate ( string calldata fromSymbol, string calldata  toSymbol, string calldata venue, uint256 amount ) external view returns ( uint256 );
+  function getTokenDecimalCount ( address tokenAddress ) external view returns ( uint256 );
+  function getTokenAddress ( string calldata  symbol ) external view returns ( address );
+  function getSynthBytes32 ( string calldata  symbol ) external view returns ( bytes32 );
+  function getForexAddress ( string calldata symbol ) external view returns ( address );
+  function arb(address  fundsReturnToAddress,  address liquidityProviderContractAddress, string[] calldata   tokens,  uint256 amount, string[] calldata  exchanges) external payable returns (bool);
+}
+
 
 contract AMP{
 
-  using SafeMath
-    for uint256;
+    using SafeMath for uint256;
+  
+    // orfeed interface
+    OrFeedInterface orfeed;
+    
+    // the owner of the contract
+    address owner;
+    
+    // Represents a single asset
+    struct Asset {
+    
+        address tokenAddress;
+        string title;
+        string description;
+        string entityName;
+        uint256 proposedMarketCapUSD;
+        string fileHash1;
+        string fileHash2;
+        string photo1URL;
+        string photo2URL;
+        string photo3URL;
+        bool assetActive;
+        string inActiveStatusReason;
+    }
+    
+    
+    // a mapping of assets in the contract
+    mapping (bytes32 => Asset) assets;
+    
+
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert();
+        }
+        _;
+    }
+    
+    function setOrfeedAddress(address orfeedAddress) onlyOwner external {
+        orfeed = OrFeedInterface(orfeedAddress);
+    }
+    
+    
 
     function getCreditScore(address entityAddress) public view returns (uint256 score){
 
     }
 
     function getPrice(address tokenAddress, uint256 amount, uint256 side, string memory venue) public view returns (uint256 price){
+        bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
@@ -45,18 +100,23 @@ contract AMP{
 
 
     function getProposedYield(address tokenAddress) public view returns (uint256 percentage){
+      bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
     function getHistoricYield(address tokenAddress, uint256 years) public view returns (uint256 percentage){
+        bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
     function getProposedMarketCap(address tokenAddress) public view returns (uint256 marketCap){
+        bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
+        return assets[assetId].proposedMarketCapUSD;
 
     }
 
     function getAssetPredictionScoreAndSize(address tokenAddress)  public view returns (uint256 score, uint256 totalPredictionMarketSize){
+        bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
@@ -65,16 +125,19 @@ contract AMP{
     }
 
     function getPredictionBalance(address predictorAddress, address tokenAddress) public view returns (uint256 currentBalance, uint256 side){
+        bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
 
     }
 
 
     function isAssetOnAMP(address tokenAddress)  public view returns (bool isOn){
+        bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
     function isPredictionMarketResolved(address tokenAddress)  public view returns (bool isOn){
+        bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
@@ -88,48 +151,53 @@ contract AMP{
     }
 
     function predictForOrAgainst(address tokenAddress, bool predictFor, uint256 amountStableCoinStaked) public returns(bool){
+        bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
     function resolvePredictionMarketAsset(address tokenAddress)  public returns(bool resolved){
+        bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
+    
 
     function addAssetToMarket(address tokenAddress, string memory title, string memory description, string memory entityName, uint256 proposedMarketCapUSD, string memory fileHash1, string memory fileHash2, string memory photo1URL, string memory photo2URL, string memory photo3URL ) public {
-
+        bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
+        assets[assetId] = Asset(tokenAddress, title, description, entityName, proposedMarketCapUSD, fileHash1, fileHash2, photo1URL, photo2URL, photo3URL, true, "");
     }
 
 
     function inactivateAsset(address tokenAddress, string memory reasonGivenByAdminOrCreator ) public {
-
+       bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
+       assets[assetId].assetActive = false;
+       assets[assetId].inActiveStatusReason = reasonGivenByAdminOrCreator;
     }
 
     function claimPredictionRewards(address tokenAddress) public returns(uint256 amountReturned){
+       bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
     function claimAssetRewards(address tokenAddress) public returns(uint256 amountReturned){
+       bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
     function claimAssetSaleRewards(address tokenAddress) public returns(uint256 amountReturned){
+       bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
     function contributeAssetRegularRewards(address tokenAddress, uint256 amountUSD) public returns(bool){
+       bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
     function contributeAssetSaleRewards(address tokenAddress, uint256 amountUSD) public returns(bool){
+       bytes32 assetId =  keccak256(abi.encodePacked(tokenAddress));
 
     }
 
-
-
-
-
-
 }
-
 
 
 
@@ -158,3 +226,4 @@ library SafeMath {
         return c;
     }
 }
+    
